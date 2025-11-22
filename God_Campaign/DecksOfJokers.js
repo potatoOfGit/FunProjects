234384e9d888e4b7b0ed052e3c5b30393beeaf48
potatoOfGit <isaachuberdeau@gmail.com>
2025-11-22 03:16:43 -0500

    Made it so card images are linked to whats pulled

--- God_Campaign/DecksOfJokers.js
+++ God_Campaign/DecksOfJokers.js
@@ -3,7 +3,67 @@ const shuffleButton = document.getElementById("shuffle");
 
 const suites = ["Hearts", "Clubs", "Diamonds", "Spades"];
 const values = ["Joker", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+const imgLinks = ["./cards/Card-Back.png",//Hearts
+                  "./cards/Two_Of_Hearts.png",
+                  "./cards/Three_Of_Hearts.png",
+                  "./cards/Four_Of_Hearts.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png", //Clubs Begin
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",//Diamons Begin
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",//Spades
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+                  "./cards/Card-Back.png",
+];
 let deck = [];
+const deckMap = new Map();
+
 
 drawButton.addEventListener('click', () => {
   const text = document.getElementById("text");
@@ -19,10 +79,28 @@ drawButton.addEventListener('click', () => {
   const drawnCardTwo = deck.pop();
   text.innerHTML = drawnCard + " (Cards remaining: " + deck.length + ")";
   textTwo.innerHTML = drawnCardTwo + " (Cards remaining " + deck.length + ")";
+  changeImage(drawnCard, drawnCardTwo);
 });
 
-function SelectDrawnCard(){
+function changeImage(cardOne, cardTwo){
+   const imgElementOne = document.getElementById("chosen-card-one");
+   const imgElementTwo = document.getElementById("chosen-card-two");
 
+   imgElementOne.src = deckMap.get(cardOne);
+   imgElementTwo.src = deckMap.get(cardTwo);
+}
+
+function reShuffle(){
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+function makeMap(deck){
+  for(let i = 0; i < deck.length; i++){
+    deckMap.set(deck[i], imgLinks[i]);
+  }
 }
 
 shuffleButton.addEventListener('click', () => {
@@ -35,6 +113,9 @@ shuffleButton.addEventListener('click', () => {
       deck.push(value + " of " + suite);
     }
   }
+
+  //Creates map between cards and imgs
+  makeMap(deck);
   
   // Shuffle using Fisher-Yates algorithm
   for (let i = deck.length - 1; i > 0; i--) {
